57206feaa302cc989b4705a0ac5bed2404b4eb59
Quasar Wei <quasar.wei@outlook.com>
2019-09-27 11:35:42 -0700

    before deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,69 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
-app.use(morgan('common'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+app.use(helmet());    // helmet needs to be before cors
+app.use(cors());
 
-app.use((req, res) => {
-  res.send('Goodbye, world!');
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  //move to the next middleware
+  next();
 });
 
-const PORT = 8000;
+const validTypes = ['Bug', 'Dark', 'Dragon',
+  'Electric', 'Fairy', 'Fighting', 'Fire',
+  'Flying', 'Ghost', 'Grass', 'Ground',
+  'Ice', 'Normal', 'Poison', 'Psychic',
+  'Rock', 'Steel', 'Water'];
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
+
+  if (req.query.name) {
+    response = response.filter(pokemon => 
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase()));
+  }
+
+  if (req.query.type) {
+    response = response.filter(pokemon => 
+      pokemon.type.includes(req.query.type));
+  }
+
+  res.json(response);
+}
+
+
+app.get('/types', handleGetTypes);
+app.get('/pokemon', handleGetPokemon);
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500.json(response);
+});
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
 });
